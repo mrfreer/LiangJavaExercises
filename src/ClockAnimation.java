06fa3433805ab748f1c119a763db16c5fede9d4d
shaggy0798 <david.freer@gmail.com>
2017-03-13 15:16:38 -0400

    BigDecimal approximating the value of e. Clock animation from Liang Java book.

---
+++ src/ClockAnimation.java
@@ -0,0 +1,29 @@
+/**
+ * Created by dfreer on 3/8/2017.
+ */
+/*
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.util.Duration;
+
+public class ClockAnimation extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        ClockPane clock new ClockPane();
+
+        EventHandler<ActionEvent> eventHandler = e-> {
+            clock.setCurrentTime();
+        };
+
+        Timeline animation = new Timeline(new KeyFrame(Duration.millis(1000), eventHandler));
+        animation.
+    }
+
+
+}
+*/
---
+++ src/DisplayClock.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+/**
+ * Created by dfreer on 3/8/2017.
+ */
+public class DisplayClock extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        ClockPane clock = new ClockPane();
+        String timeString = clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond();
+        Label lblCurrentTime = new Label(timeString);
+        BorderPane pane = new BorderPane();
+        pane.setCenter(clock);
+        pane.setBottom(lblCurrentTime);
+        BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+
+        Scene scene = new Scene(pane, 250, 250);
+        primaryStage.setTitle("Display Clock");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
